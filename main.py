c2535f70e8f44c0f5187dfbacc18c452042061dc
Andrés Court <andres@andrescourt.com>
2022-09-23 09:39:37 -0500

    Feature: Main Screen Created an empty main screen with a title and the specified background color

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+# --------------- IMPORTS ---------------------#
+from tkinter import *
+
+# --------------- CONSTANTS ---------------------#
 BACKGROUND_COLOR = "#B1DDC6"
 
+# --------------- WINDOW MANAGEMENT ---------------------#
+window = Tk()
+window.title("Flashy")
+window.config(bg=BACKGROUND_COLOR)
+
+window.mainloop()
